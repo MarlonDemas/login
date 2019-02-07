d58d3083ff4ec25a83a0c4dbd513f0303254a645
MarlonDemas <demasmarlon@gmail.com>
2019-02-07 11:07:08 +0200

    added title for sign up page and created a hotel bookings page

---
+++ hotel.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Hotel Booking Form</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
+        <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+        <style>
+            .hero {
+                background-image: url("img/pexels-photo-189296.jpeg");
+                background-size: cover;
+                background-repeat: no-repeat;
+            }
+        </style>
+    </head>
+    <body>
+        <section class="hero is-fullheight has-background">
+        </section>
+    </body>
+</html>
--- signup.php
+++ signup.php
@@ -41,7 +41,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Login</title>
+        <title>Sign Up</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
         <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
         <style>
